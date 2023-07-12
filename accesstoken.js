0065ffe40ccbed0202938344b395c98b5e9dfbf2
Leonel <leonel.h.oliveira@exxonmobil.com>
2023-07-12 12:36:48 -0300

    Formating

--- accesstoken.js
+++ accesstoken.js
@@ -1,18 +1,18 @@
 const axios = require('axios')
 const clientId = ""
-const clientSecret = "" 
+const clientSecret = ""
 
-async function getAcessToken(){
+async function getAcessToken() {
     const url = `https://login.microsoftonline.com/${tenantID}/oauth2/token`
     const identify = '499b84ac -1321 -427f -aa17 -267ca6975798'
     const data = `grand_type=client_credentials$client_id=${clientId}&client_secret=${clientSecret}&resource=${identify}`;
 
-    try{
+    try {
         const response = await axios.post(url, data);
         const accessToken = response.data.acess_token;
         return accessToken
 
-    } catch(error) {
+    } catch (error) {
         console.error('Error 401 - Unauthorized', error);
         throw error;
     }
--- text.js
+++ text.js
@@ -5,39 +5,37 @@ const Project = 'Fuels%20IT'
 const azureDevOpsURL = `https://analytics.dev.azure.com/${Organization}/${Project}`
 
 // function consult to a query 
-async function fetchData(){
-/*  const params = [   //queryone
-    'Depth eq 2 and StartDate ne null',
-    'IterationName,StartDate,EndDate',
-    'StartDate'
-] */
+async function fetchData() {
+    /*  const params = [   //queryone
+        'Depth eq 2 and StartDate ne null',
+        'IterationName,StartDate,EndDate',
+        'StartDate'
+    ] */
     const params = [  //querytwo
-    "IterationLevel3 eq '2023 I2' and IterationLevel2 eq '2023' and Depth eq 3 and StartDate ne null",
-    'IterationLevel2,IterationName,StartDate,EndDate',
-    'StartDate'
-]
-
+        "IterationLevel3 eq '2023 I2' and IterationLevel2 eq '2023' and Depth eq 3 and StartDate ne null",
+        'IterationLevel2,IterationName,StartDate,EndDate',
+        'StartDate'
+    ]
     const url = `${azureDevOpsURL}/_odata/v3.0/Iterations/?$filter=${params[0]}&$select=${params[1]}&$orderby=${params[2]}`
     const auth = {
-    username: "",
-    password: "" //putyourtokenherepls
-  }
+        username: "",
+        password: "" //putyourtokenherepls
+    }
 
-  try{
-    const response = await axios.get(url, {
-      headers: {
-        'Content-Type': 'application/json',
-        'Authorization': "Basic " + window.btoa(":" + auth.password)
-      }
-    });
-    const data = response.data;
-    return data;
-  } catch(error){
-    console.error('Error 401', error);
-    throw error;
+    try {
+        const response = await axios.get(url, {
+            headers: {
+                'Content-Type': 'application/json',
+                'Authorization': "Basic " + window.btoa(":" + auth.password)
+            }
+        });
+        const data = response.data;
+        return data;
+    } catch (error) {
+        console.error('Error 401', error);
+        throw error;
+    }
 }
-}
-
 // query for a post method
 /* const query = {
     "$filter": "Depth eq 2 and StartDate ne null",
@@ -46,13 +44,13 @@ async function fetchData(){
 } */
 
 // displaying the data in console.log
-function displayData(data){
+function displayData(data) {
     console.log('Database:')
     console.log(data)
 }
 
-async function fecthDataAndDisplay(){
-    try{
+async function fecthDataAndDisplay() {
+    try {
         const data = await fetchData()
         displayData(data);
     } catch (error) {
